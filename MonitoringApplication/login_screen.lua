4a650fd75167cd63d1f58146051576a5cd35b8ff
Lpaterson441 <36324784+Lpaterson441@users.noreply.github.com>
2018-10-12 19:14:59 +0800

    Update login_screen.lua
    Updated to ensure that all code is in scenes, allowing transitioning between different pages

--- MonitoringApplication/login_screen.lua
+++ MonitoringApplication/login_screen.lua
@@ -1,20 +1,85 @@
 local composer = require("composer")
 local scene = composer.newScene()
 local widget = require("widget")
-local submitButton
-local nameField, phoneField, emailField
-
-
-
-
-words = display.newText("Welcome to the security monitoring app",250,0, "Arial", 17)
-
-words.x = display.contentCenterX
+local sumbitButton, nameField, phoneField, emailField, words
 
 
 
 
 
+function scene:create(event)
+	sceneGroup = self.view
+	
+	words = display.newText("Welcome to the security monitoring app",250,0, "Arial", 17)
+	
+	
+	
+	userName = display.newText("Name", 160,120,"Arial",14)
+	nameField =  native.newTextField(160,150,200,30)
+	
+	
+	phoneNum = display.newText("Phone Number",160, 190,"Arial",14)
+	phoneField = native.newTextField(160,220,200,30)
+	
+	email = display.newText("Email", 160, 260, "Arial",14)
+	emailField = native.newTextField(160,290, 200,30)
+	
+	submitButton = widget.newButton(
+	{
+		left = 100,
+		top = 200,
+		id = "subButton",
+		label = "Submit",
+		onEvent = handlesubButtonEvent
+		}
+	)
+
+	
+	words.x = display.contentCenterX
+	sceneGroup:insert(words)
+	
+	nameField.align = "left" -- aligns text not box itself
+	sceneGroup:insert(nameField)
+	sceneGroup:insert(userName)
+	
+	phoneField.align = "left"
+	phoneField.inputType = "number" -- Ensures numeric input (corona gives popup message if anything else is entered)
+	sceneGroup:insert(phoneField)
+	sceneGroup:insert(phoneNum)
+	
+	
+	emailField.align = "left"
+	sceneGroup:insert(emailField)
+	sceneGroup:insert(email)
+	
+	submitButton.x = display.contentCenterX
+	submitButton.y = display.contentCenterY*1.5
+	sceneGroup:insert(submitButton)
+	
+	--listeners for input
+	nameField:addEventListener("userInput", textListener)
+	phoneField:addEventListener("userInput",textListener)
+	emailField:addEventListener("userInput", textListener)
+	
+end
+
+function scene:show(event)
+
+	local phase = event.phase
+	if "did" == phase then
+	end
+end
+
+function scene:hide(event)
+	local phase = event.phase
+	
+	if "will" == phase then
+	
+	end
+end
+
+function scene:destroy(event)
+end
 
 
 function textListener( event ) -- Code to create text listener
@@ -29,42 +94,40 @@ function textListener( event ) -- Code to create text listener
 	elseif (event.phase == "editing") then
 		print( event.newCharacters)
 		print( event.oldText)
-		print(event.startPosition)
-		print(event.text)
+		print( event.startPosition)
+		print( event.text)
 		print("TESTING") -- Tests if editing phase is running correctly
 	
 	
 	end
 end 
 
+function handlesubButtonEvent(event)
+
+	if (event.phase == "ended") then
+		composer.gotoScene("loading_screen",fade,1000)
+		return true
+	end
+end
 
 
---userName code
-userName = display.newText("Name", 160,120,"Arial",14)
-nameField =  native.newTextField(160,150,200,30)
-nameField.align = "left" -- aligns text not box itself
+	
 
 
---Phone number code
-phoneNum = display.newText("Phone Number",160, 190,"Arial",14)
-phoneField = native.newTextField(160,220,200,30)
-phoneField.align = "left"
-phoneField.inputType = "number" -- Ensures numeric input (corona gives popup message if anything else is entered)
 
---Email code
-email = display.newText("Email", 160, 260, "Arial",14)
-emailField = native.newTextField(160,290, 200,30)
-emailField.align = "left"
 
--- Event listener code, gets user input
-nameField:addEventListener("userInput", textListener)
-phoneField:addEventListener("userInput",textListener)
-emailField:addEventListener("userInput", textListener)
 
---Variables that store user input for saving and later use
-namePut = nameField.text
-phonePut = phoneField.text
-emailPut = emailField.text
+
+
+--Listener code
+scene:addEventListener("create", scene)
+scene:addEventListener("show",scene)
+scene:addEventListener("hide",scene)
+scene:addEventListener("destroy",scene)
+
+
+
+
 
 
 return scene
