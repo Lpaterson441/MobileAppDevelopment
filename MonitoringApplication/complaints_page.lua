a1df18bf3876b9cfba71695a5776312ffa09a0df
Romilkajaria <42332954+Romilkajaria@users.noreply.github.com>
2018-10-18 02:21:39 +0800

    Update complaints_page.lua
    added structure to the complaints page. added remove scene = main menu

--- MonitoringApplication/complaints_page.lua
+++ MonitoringApplication/complaints_page.lua
@@ -6,4 +6,47 @@
 local composer = require "composer"
 local scene = composer.newScene()
 local widget = require "widget"
--- load first scene
+
+function scene:create( event )
+	
+	
+end
+
+function scene:show( event )
+	
+	local phase = event.phase
+	
+	if "did" == phase then
+	
+	composer.removeScene( "menu_screen" )
+	end
+	
+end
+
+function scene:hide( event )
+	
+	local phase = event.phase
+	
+	if "will" == phase then
+	
+		
+	
+	end
+	
+end
+
+function scene:destroy( event )
+	
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+---------------------------------------------------------------------------------
+
+return scene
