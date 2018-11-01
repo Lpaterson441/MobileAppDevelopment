3ffaf21ebf198d578fb08d228eca76492fd55cb8
Lpaterson441 <36324784+Lpaterson441@users.noreply.github.com>
2018-11-01 15:57:38 +0800

    Update Security_Monitoring_page.lua
    Back button added

--- MonitoringApplication/Security_Monitoring_page.lua
+++ MonitoringApplication/Security_Monitoring_page.lua
@@ -7,7 +7,35 @@ local composer = require "composer"
 local scene = composer.newScene()
 local widget = require "widget"
 
+local backButton
+
+local function onSceneTouch(self,event) -- Function for back button to return to homescreen
+	if event.phase == "ended" then
+		composer.gotoScene("menu_screen","slideLeft",800)
+		
+		return true
+	end
+end
+
+
 function scene:create( event )
+	local sceneGroup = self.view
+		backButton = widget.newButton(
+	{
+		top = display.contentHeight,
+		id = "backButton",
+		label = "back"
+		}
+	)
+	
+	backButton.x = display.contentCenterX 
+	backButton.y = -10
+	sceneGroup:insert(backButton)
+	
+	backButton.touch = onSceneTouch
+
+	
+	
 	
 	
 end
